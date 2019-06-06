79e7d9807e41c6706a46474a070cfc1723817a62
Lê Như Phương <43329732+Lephuong96@users.noreply.github.com>
2019-06-06 14:11:36 +0700

    update index.php

--- index.php
+++ index.php
@@ -1,19 +1,33 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Class Rectangle</title>
+    <style type="text/css">
+        .content {
+            width: 100%;
+            color: blue;
+            font-size: 30px;
+        }
+    </style>
+</head>
+<body>
+<div class="content">
 <?php
 include "Rectangle.php";
 $width  = 10;
 $height = 20;
 $rectangle = new Rectangle($width, $height);
 
-//echo $rectangle->width; //  10
-//echo $rectangle->height; //  20
-
-$rectangle->height = 40;
-$rectangle->width = 20;
-
-//echo $rectangle->height; //  30
-//echo $rectangle->width; //  20
-
-echo ( "<br>Perimeter:".$rectangle->getPerimeter()); //  100
-echo ( "<br>Area:".$rectangle->getArea()); //  600
-
-echo ( "<br>Your Rectangle". $rectangle->display());
+echo "Width Rectangle:" .$rectangle->width. "<br>";
+echo "Height Rectangle:" .$rectangle->height;
+echo "<br>Perimeter:".$rectangle->getPerimeter();
+echo "<br>Area:".$rectangle->getArea();
+echo ("<br>Your Rectangle:". $rectangle->display());
+?>
+</div>
+</body>
+</html>
